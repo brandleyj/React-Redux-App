169e9443fd8f5a0e6cec705c0cd4ed03a2704d80
Justin Brandley <brandleyj@gmail.com>
2019-10-10 16:10:22 -0500

    initial push

---
+++ react-redux-app/src/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import "./index.css";
+import App from "./App";
+
+const store = createStore(rootReducer);
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	rootElement
+);
